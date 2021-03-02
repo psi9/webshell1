34c13bce8d7d250dd5249c68aa1bf67bdf3675c1
psi9 <semyon.pavlenko@gmail.com>
2021-03-02 15:29:50 +1000

    111

--- webshell1/webshell-client/src/components/ShellForm.js
+++ webshell1/webshell-client/src/components/ShellForm.js
@@ -19,8 +19,8 @@ class ShellForm extends React.Component {
             .then(res => res.json())
             .then(res => this.setState({
                 items: res,
-                max: res.length,
-                current: res.length,
+                max: res.length - 1,
+                current: res.length - 1,
                 input: (res.map(item => item.input))[res.length - 1],
                 output: (res.map(item => item.output))[res.length - 1]
             }))
@@ -40,23 +40,29 @@ class ShellForm extends React.Component {
         })
             .then(res => res.json())
             .then(res => this.setState({
-                items: [...res.items, res],
-                max: res.id,
-                current: res.id
+                items: [...this.state.items, res],
+                max: res.id - 1,
+                current: res.id - 1,
+                output: res.output
             }))
             .catch(err => console.log(err));
     }
     onKeyDown = e => {
         if (e.key === "ArrowUp" && this.state.current > 1) {
             this.setState(previous => ({
-                current: previous.current - 1
+                current: previous.current - 1,
+                input: (this.state.items.map(item => item.input))[this.state.current - 1],
+                output: (this.state.items.map(item => item.output))[this.state.current - 1]
             }));
 
         }
         else if (e.key === "ArrowDown" && this.state.current < this.state.max) {
             this.setState(previous => ({
-                current: previous.current + 1
+                current: previous.current + 1,
+                input: (this.state.items.map(item => item.input))[this.state.current + 1],
+                output: (this.state.items.map(item => item.output))[this.state.current + 1]
             }));
+
 
         }
         
